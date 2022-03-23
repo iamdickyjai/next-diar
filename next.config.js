2c61ab91fc2904f007a22d4828ad6f6dc88df866
iamdickyjai <soyiufai@gmail.com>
2022-03-24 04:35:00 +0800

    Finished home page skeleton

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   distDir: 'build',
+  env: {
+    // url: "https://api-6rvxylw3fq-de.a.run.app"
+    url: "http://127.0.0.1:5000"
+  }
   // serverRuntimeConfig: {
   //   // Will only be available on the server side
   //   PROJECT_ROOT: __dirname,
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,254 @@
 import React from 'react';
 import cn from 'classnames';
+import toast, { Toaster } from 'react-hot-toast';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [selected, setSelect] = React.useState("");
+  const [result, setResult] = React.useState();
+  const [isAllow, setAllow] = React.useState(false);
+  const [isDone, setDone] = React.useState(false);
 
+  // Control the appropriate toast to be showed when app selection is dis/enable
+  React.useEffect(() => {
+    let toastId;
+    if (isAllow) {
+      toastId = toast.loading("Processing...", { id: "loading" });
+    } else {
+      toast.dismiss(toast.loading("", { id: "loading" }));
+    }
+  }, [isAllow])
+
+  // Action when diarization is completed
+  React.useEffect(() => {
+    if (result) {
+      console.log(result);
+
+      toast.remove();
+
+      setDone(true);
+      toast.success("Done!", { duration: 2000 })
+    }
+  }, [result])
+
+  // Submit the result to corresponding application
+  const submit = () => {
+    if (selected == "") {
+      toast.error("You need to choose a feature first!", { duration: 2000 });
+    } else {
+      // TODO Navigate to according page
+    }
+  }
+
+  const reset = () => {
+    setSelect("");
+    setResult(null);
+    setAllow(false);
+    setDone(false);
+
+    toast.success("Reset!", { duration: 2000 })
+  }
 
   return (
-    <>
-      <Form />
-    </>
+    <div className={styles.container}>
+      <Toaster />
+      <FileSelection setResult={setResult} setAllow={setAllow} disabled={isAllow} />
+      <AppSelection setSelect={setSelect} disabled={!isAllow} />
+      {isDone &&
+        <>
+          <button onClick={submit}>GO</button><button onClick={reset}>Reset</button>
+        </>}
+    </div>
   )
 }
 
-function Form() {
-  const registerUser = async event => {
-    event.preventDefault()
+function FileSelection(props) {
+  const [link, setLink] = React.useState("");
+  const fileRef = React.createRef();
+  const [isFile, setDecision] = React.useState(false);
 
-    const path = event.target.file;
+  const setResult = props.setResult;
+  const setAllow = props.setAllow;
+  const disabled = props.disabled;
 
-    const formData = new FormData();
-    formData.append("file", path.files[0]);
+  // When user typed in link, remove file
+  const chooseLink = (event) => {
+    setLink(event.target.value)
+    fileRef.current.value = null;
+    setDecision(false);
+  }
 
+  // When file is submitted, remove link
+  const chooseUpload = () => {
+    setLink("");
+    setDecision(true);
+  }
+
+  // Handler for button
+  const submit = async () => {
+    if (!link && !fileRef.current.value) {
+      toast.error("You must choose one method!", { id: "error", duration: 2000 });
+      return;
+    }
+
+    if (link) {
+      if (!isValidHttpUrl(link)) {
+        toast.error("That is not a valid Link!", { id: "error", duration: 2000 });
+        return
+      }
+    }
+
+    fetchTimeStamp();
+  }
+
+  // Send POST request to server
+  // Success: Arrays of timestamp
+  // Failed: Corresponding status code
+  const fetchTimeStamp = async () => {
+    let request;
+    let content;
+    let config;
+
+    // Disable pointer event 
+    setAllow(true);
+
+    // Prepare setting for fetch according to decision user made
+    if (isFile) {
+      request = `${process.env.url}/`;
+
+      const path = fileRef.current.files;
+
+      const formData = new FormData();
+      formData.append("file", path[0]);
+      content = formData;
+
+      config = {
+        'Access-Control-Allow-Origin': '*',
+      }
+    } else {
+      request = `${process.env.url}/youtube`;
+      content = JSON.stringify({ "url": link });
+
+      config = {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      }
+    }
+
+    // SEND POST REQUEST HERE
     try {
-      const res = await fetch(`https://api-6rvxylw3fq-de.a.run.app/`, {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
+      const response = await fetch(request, {
+        method: "POST",
+        body: content,
+        headers: config,
       })
-      // const res = await fetch(`http://127.0.0.1:5000/`, {
-      //   method: 'POST',
-      //   body: formData,
-      //   headers: {
-      //     'Access-Control-Allow-Origin': '*'
-      //   }
-      // })
 
-      const result = await res.json();
-      console.log(result);
-      // result.user => 'Ada Lovelace'
-    } catch (err) {
-      alert(err);
+      // Wait for response
+      // Success: return result
+      // Failed: show error and enable inputSelection again.
+      const result = await response.json();
+      if (response.status == 200) {
+        setResult(result);
+      } else {
+        switch (response.status) {
+          case 500:
+            toast.error("Server error occured", { id: "error", duration: 2000 });
+            break;
+          case 403:
+            toast.error("Cannot download audio from this link!", { id: "error", duration: 2000 });
+            break;
+          default:
+            toast.error("Unknown error", { id: "error", duration: 2000 });
+            break;
+        }
+
+        setAllow(false);
+      }
+    } catch (error) {
+      toast.error("Unexpected error occurred, please try again later.", { id: 'error' });
+
+      setAllow(false);
     }
   }
 
   return (
-    <form onSubmit={registerUser}>
-      <label htmlFor="name">Name</label>
-      <input id="name" name="file" type="file" accept='audio/*' />
-      <button type="submit" name='upload'>Register</button>
-      <input id="link" name="link" type="text" />
-      <button type="submit" name='upload'>Register</button>
-    </form>
+    <div className={cn(styles.inputContainer, { [styles.disabled]: disabled })}>
+      <input type="text" className={styles.inputUrl} value={link} onChange={chooseLink} />
+      <span>or</span>
+      <input type="file" onChange={chooseUpload} ref={fileRef} />
+      <button onClick={submit}>Go</button>
+    </div>
   )
+}
+
+function AppSelection(props) {
+  const setSelect = props.setSelect;
+  const disabled = props.disabled;
+
+  const [blockArr, setBlock] = React.useState([
+    { type: "extract", id: 0, check: false },
+    { type: "translate", id: 1, check: false },
+    { type: "conference", id: 2, check: false }
+  ]);
+
+  const selectHandler = (id) => {
+    // Get the unselected option, and set them to false
+    const arrWithoutTheSelected = blockArr.filter(x => x.id !== id);
+    arrWithoutTheSelected.forEach(ele => ele.check = false);
+
+    // Get the selected option, then select/unselect it
+    const arrWithTheID = blockArr.filter(x => x.id === id);
+    arrWithTheID.forEach(ele => {
+      ele.check = !ele.check;
+      ele.check ? setSelect(ele.type) : setSelect("");
+    });
+
+    // Concat two arrays into one again, then sort them according to id, so the order will not change
+    const combinedArr = arrWithoutTheSelected.concat(arrWithTheID);
+    const sortedArr = combinedArr.sort((x, y) => x.id - y.id);
+
+    setBlock(sortedArr);
+  }
+
+  return (
+    <div className={styles.selectionArea}>
+      {blockArr.map(x => {
+        return <SelectBlock type={x.type} check={x.check}
+          id={x.id} key={x.id} handler={selectHandler} disabled={disabled} />
+      })}
+    </div>
+  )
+}
+
+function SelectBlock(props) {
+  const type = props.type;
+  const check = props.check;
+  const id = props.id;
+  const handler = props.handler;
+  const disabled = props.disabled;
+
+  return (
+    <div
+      className={cn(styles.block, { [styles.selected]: check }, { [styles.disabled]: disabled })}
+      onClick={() => handler(id)}
+    >
+      <h1>{type}</h1>
+      <p>{"TODO: "}</p>
+      <li>{"諗下如果轉緊個陣refresh會點?例如refresh前話比server知唔駛再做?"}</li>
+      <li>{"!!!讀useRouter, 好似好有用"}</li>
+      <li>{"個block都要加個effect, 例如hover會enlarge/變色"}</li>
+      <div>Go&gt;&gt;&gt;</div>
+    </div>
+  )
+}
+
+function isValidHttpUrl(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  return !!pattern.test(str);
 }
