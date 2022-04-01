aee5056214ad9189a1a0611dea3ff408ef4e2c96
iamdickyjai <soyiufai@gmail.com>
2022-04-01 15:43:42 +0800

    Checkpoint

--- components/player.js
+++ components/player.js
@@ -5,6 +5,9 @@ import 'rc-slider/assets/index.css';
 export default function Player(props) {
   const audio = props.audio;
   const timestamp = props.timestamp;      // TODO: Allow user to adjust this
+  const index = props.index;
+  const switchPlaying = props.switchPlaying;
+  const playingKey = props.playingKey;
 
   // Constant state
   const duration = timestamp[1] - timestamp[0];
@@ -26,9 +29,24 @@ export default function Player(props) {
     }
   }, [counter])
 
+  React.useEffect(() => {
+    if (isPaused && source.current) {
+      // TODO: Cancel all the Audio context and setInterval()
+      source.current.stop();
+
+      clearTimeout(intervalId.current);
+    }
+  }, [isPaused])
+
+  React.useEffect(() => {
+    playingKey.current !== index && setPaused(true);
+  }, [playingKey.current]);
+
   const handlePlayPause = () => {
     if (isPaused) {
       // TODO: Play the audio
+      switchPlaying(index);
+
       fileReader.onload = function () {
         ;
         audioCtx.current.decodeAudioData(
@@ -41,6 +59,11 @@ export default function Player(props) {
             source.current.start(audioCtx.current.currentTime,
               timestamp[0] + counter,
               timestamp[1] - (timestamp[0] + counter));
+            source.current.onended = () => {
+              // !Ended cannot replay
+              clearTimeout(intervalId.current);
+              setPaused(true);
+            }
 
             start.current = audioCtx.current.currentTime - counter;
             intervalId.current = (setTimeout(() => {
@@ -50,14 +73,6 @@ export default function Player(props) {
           function (e) { console.log("Error"); });
       };
       fileReader.readAsArrayBuffer(audio);
-
-    } else {
-      // TODO: Cancel all the Audio context and setInterval()
-      // source.disconnect();
-      // console.log(source);
-      source.current.stop();
-
-      clearInterval(intervalId.current);
     }
 
     setPaused(!isPaused);
--- pages/test/index.js
+++ pages/test/index.js
@@ -6,17 +6,25 @@ export default function Test() {
   const fileRef = React.createRef();
   const [file, setFile] = React.useState();
   const timestamp = [[0, 13], [13, 15], [18, 26]];
+  const playingKey = React.useRef();
   // const timestamp = [[10, 15]];
 
   const handleChange = () => {
     setFile(fileRef.current.files[0]);
   }
 
+  const switchPlaying = (index) => {
+    // console.log(index);
+    playingKey.current = index;
+  }
+
   return (
     <>
       <h1>Test</h1>
       <input type="file" accept='audio/*' onChange={handleChange} ref={fileRef} />
-      {file && timestamp.map(ele => <Player audio={file} timestamp={ele} />)}
+      {file && timestamp.map((ele, index) =>
+        <Player audio={file} timestamp={ele} key={index} index={index} switchPlaying={switchPlaying} playingKey={playingKey} />
+      )}
     </>
   )
 }
